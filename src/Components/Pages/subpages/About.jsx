8c9cfd6737d19d023ca98b5c805c60b6efa81a52
Ankit Verma <a.verma0827@gmail.com>
2023-06-01 04:42:18 +0530

    .

---
+++ src/Components/Pages/subpages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../CSS/About.css'
+
+const About = () => {
+  return (
+    <div className='parent__about_div'>
+        <h1>About</h1>
+    </div>
+  )
+}
+
+export default About
